54cbe7ed5b68f72015818f9737b41b0a70a11320
M.Shoaib Shafiq <shoaibxp463@gmail.com>
2022-06-26 19:20:49 +0500

    eslint issue resolved

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    quotes: [2, "single", { avoidEscape: true }],
     "react/function-component-definition": 0,
     "import/extensions": 0,
     "react/prop-types": 0,
